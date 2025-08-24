4fb6a649c9e52e69bea50935cc332022271df030
Bala-s-dev <baladev2004@gmail.com>
2025-08-24 18:35:28 +0530

    final commit

--- src/app/generate-program/page.tsx
+++ src/app/generate-program/page.tsx
@@ -11,14 +11,14 @@ const GenerateProgramPage = () => {
   const [callActive, setCallActive] = useState(false);
   const [connecting, setConnecting] = useState(false);
   const [isSpeaking, setIsSpeaking] = useState(false);
-  const [messages, setMessages] = useState<any[]>([]);
+  const [messages, setMessages] = useState<any[]>([]); 
   const [callEnded, setCallEnded] = useState(false);
 
   const { user } = useUser();
   const router = useRouter();
 
   const messageContainerRef = useRef<HTMLDivElement>(null);
-  console.log()
+  console.log();
   // SOLUTION to get rid of "Meeting has ended" error
   useEffect(() => {
     const originalError = console.error;
